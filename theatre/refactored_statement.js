16bda25c3b4de07b91c71921378b50ce13e82d0c
Tin Myo Win <tinmyowin@better.hr>
2023-05-18 22:36:20 +0630

    Removing Parameter for refactoring

--- theatre/refactored_statement.js
+++ theatre/refactored_statement.js
@@ -9,7 +9,7 @@ function statement(invoice, plays) {
         }).format;
 
     for (let perf of invoice.performances) {
-        let thisAmount = amountFor(perf, playFor(perf));
+        let thisAmount = amountFor(perf);
 
         // add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0);
@@ -20,10 +20,10 @@ function statement(invoice, plays) {
         totalAmount += thisAmount;
     }
 
-    function amountFor(aPerformance, play) {
+    function amountFor(aPerformance) {
         let result = 0;
 
-        switch (play.type) {
+        switch (playFor(aPerformance).type) {
             case "tragedy":
                 result = 40000;
                 if (aPerformance.audience > 30) {
@@ -38,7 +38,7 @@ function statement(invoice, plays) {
                 result += 300 * aPerformance.audience;
                 break;
             default:
-                throw new Error(`unknown type: ${play.type}`);
+                throw new Error(`unknown type: ${playFor(aPerformance).type}`);
         }
         return result;
     }
