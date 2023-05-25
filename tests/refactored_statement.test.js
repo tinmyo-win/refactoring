a7d79ebcd8e1be20eed3de7f0a8e2e1229ac1c77
Tin Myo Win <tinmyowin@better.hr>
2023-05-25 23:02:54 +0630

    after refactoring add htmlrendering function

--- tests/refactored_statement.test.js
+++ tests/refactored_statement.test.js
@@ -1,4 +1,4 @@
-const statement = require('../theatre/refactored_statement');
+const { statement, htmlStatement} = require('../theatre/refactored_statement');
 const invoice = require('../theatre/invoices.json')[0];
 const plays = require('../theatre/plays.json');
 
@@ -11,6 +11,22 @@ test(`testing statement for customer ${invoice['customer']}`, () => {
                     "You earned 47 credits\n"
 
     const result = statement(invoice, plays)
+
+    expect(result).toBe(expected_result);
+});
+
+test(`testing html statement for customer ${invoice['customer']}`, () => {
+    let expected_result = "<h1>Statement for BigCo</h1>\n" +
+                     "<table>\n" +
+                     "<tr><th>play</th><th>seats</th><th>cost</th></tr>" +
+                     " <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>\n" +
+                     " <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>\n" +
+                     " <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>\n" +
+                     "</table>\n" +
+                     "<p>Amount owed is <em>$1,730.00</em></p>\n" +
+                    "<p>You earned <em>47</em> credits</p>\n"
+
+    const result = htmlStatement(invoice, plays)
 
     expect(result).toBe(expected_result);
 });
