7f4ab8ce25cccd8f61dbb066b5fa7785bdead6ef
Tin Myo Win <tinmyowin@better.hr>
2023-06-08 10:51:32 +0630

    refactor Performance calculator with polimorphish

--- theatre/createStatementData.js
+++ theatre/createStatementData.js
@@ -3,28 +3,36 @@ class PerformanceCalculator {
         this.performance = aPerformance;
         this.play = aPlay;
     }
-
     get amount() {
-        let result = 0;
+        throw new Error('subclass responsibility');
+    }
 
-        switch (this.play.type) {
-            case "tragedy":
-                result = 40000;
-                if (this.performance.audience > 30) {
-                    result += 1000 * (this.performance.audience - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (this.performance.audience > 20) {
-                    result += 10000 + 500 * (this.performance.audience - 20);
-                }
-                result += 300 * this.performance.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${this.play.type}`);
+    get volumeCredits() {
+        return Math.max(this.performance.audience - 30, 0);
+    }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 40000;
+        if (this.performance.audience > 30) {
+            result += 1000 * (this.performance.audience - 30);
         }
         return result;
+    }
+}
+class ComedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 30000;
+        if (this.performance.audience > 20) {
+            result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        return result;
+    }
+    
+    get volumeCredits() {
+        return super.volumeCredits + Math.floor(this.performance.audience / 5);
     }
 }
 
@@ -38,46 +46,26 @@ module.exports = function createStatementData(invoice, plays) {
     return statementData;
 
     function enrichPerformance(aPerformance) {
-        const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance))
+        const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance);
         result.play = calculator.play;
         result.amount = calculator.amount;
-        result.volumeCredits = volumeCreditsFor(result);
+        result.volumeCredits = calculator.volumeCredits;
         return result;
+    }
+
+    function createPerformanceCalculator(aPerformance, aPlay) {
+        // return new PerformanceCalculator(aPerformance, aPlay);
+        switch (aPlay.type) {
+            case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
+            case "comedy": return new ComedyCalculator(aPerformance, aPlay);
+            default:
+                throw new Error(`unknown type: ${aPlay.type}`);
+        }
     }
 
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
-    }
-
-    function amountFor(aPerformance) {
-        let result = 0;
-
-        switch (aPerformance.play.type) {
-            case "tragedy":
-                result = 40000;
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${aPerformance.play.type}`);
-        }
-        return result;
-    }
-
-    function volumeCreditsFor(aPerformance) {
-        let result = 0;
-        result += Math.max(aPerformance.audience - 30, 0);
-        if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-        return result;
     }
 
     function totalAmount(data) {
