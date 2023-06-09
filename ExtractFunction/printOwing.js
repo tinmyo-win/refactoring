45e3a2db450241bf60190139644c5a7eb68a31c8
Tin Myo Win <tinmyowin@better.hr>
2023-06-09 13:42:44 +0630

    extract outstanding function

--- ExtractFunction/printOwing.js
+++ ExtractFunction/printOwing.js
@@ -1,22 +1,63 @@
 function printOwing(invoice) {
-    printBanner();
-    let outstanding = calculateOutstanding();
-    printDetails(outstanding);
 
-    function printDetails(outstanding) {
-        //print details
-        console.log(`name: ${invoice.customer}`);
-        console.log(`amount: ${outstanding}`);
-    }
+    printBanner();
+
+    const outstanding = calculateOutstanding(invoice);
+
+    recordDueDate(invoice)
+
+    printDetails(invoice, outstanding)
 }
 
-function calculateOutstanding() {
-    return 300;
+function recordDueDate(invoice) {
+    // record due date
+    let Clock = {};
+    Clock.today = new Date();
+    const today = Clock.today;
+    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+}
+
+function calculateOutstanding(invoice) {
+    let result = 0;
+
+    for (const o of invoice.orders) {
+        result += o.amount;
+    }
+
+    return result;
 }
 
 function printBanner() {
-    console.log('Pirnt Banner');
+    console.log("***********************");
+    console.log("**** Customer Owes ****");
+    console.log("***********************");
 }
 
-printOwing({ customer: "Mg Mg" })
+function printDetails(invoice, outstanding) {
+    //print details
+    console.log(`name: ${invoice.customer}`);
+    console.log(`amount: ${outstanding}`);
+    console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
+}
+
+const invoice = {
+    customer: "Mg Mg",
+    orders: [
+        {
+            id: 1,
+            amount: 3
+        },
+        {
+            id: 2,
+            amount: 4
+        },
+        {
+            id: 3,
+            amount: 5
+        },
+
+    ]
+}
+
+printOwing(invoice)
 
