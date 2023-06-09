98741bf3721b92be6ad24cac1c45cb9baefbc635
Tin Myo Win <tinmyowin@better.hr>
2023-06-09 13:19:09 +0630

    update Province.test

--- tests/Province.test.js
+++ tests/Province.test.js
@@ -1,6 +1,75 @@
 const { Province, sampleProvinceData } = require('../PlanManipulator/backend/Province')
-test('province', () => {
-    const asia = new Province(sampleProvinceData());
 
-    expect(asia.shortfall).toBe(5);
-});
+describe('province', function () {
+    let asia;
+    beforeEach(() => {
+        asia = new Province(sampleProvinceData());
+    });
+
+    test('shortfall', () => {
+        expect(asia.shortfall).toBe(5);
+    });
+
+    test('profit', () => {
+        expect(asia.profit).toBe(230);
+    });
+
+    test('change production', () => {
+        asia.producers[0].production = 20;
+        expect(asia.shortfall).toBe(-6);
+        expect(asia.profit).toBe(292);
+    });
+
+    test('zero demand', () => {
+        asia.demand = 0;
+        expect(asia.shortfall).toBe(-25);
+        expect(asia.profit).toBe(0);
+    });
+
+    test('negative demand', () => {
+        asia.demand = -1;
+        expect(asia.shortfall).toBe(-26);
+        expect(asia.profit).toBe(-10);
+    });
+
+    test('empty string demand', () => {
+        asia.demand = "";
+        expect(asia.shortfall).NaN;
+        expect(asia.profit).NaN;
+    })
+
+    test('string for producers', () => {
+        const data = {
+            name: "String producers",
+            producers: "",
+            demand: 30,
+            price: 20
+        };
+
+        const prov = new Province(data);
+        expect(prov.shortfall).toBe(0);
+    })
+})
+
+describe('no producers', function () {
+    let noProducers;
+
+    beforeEach(() => {
+        const data = {
+            name: "No proudcers",
+            producers: [],
+            demand: 30,
+            price: 20
+        };
+        noProducers = new Province(data);
+    })
+
+    test('shortfall', function () {
+        expect(noProducers.shortfall).toBe(30);
+    })
+
+    test('profit', function () {
+        expect(noProducers.profit).toBe(0);
+    })
+
+})
