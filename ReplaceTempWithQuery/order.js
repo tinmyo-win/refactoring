ab036e1b0f68a553b1c9eced70ba61a1515c6031
Tin Myo Win <tinmyowin356@gmail.com>
2023-06-23 21:13:15 +0630

    Refactoring with Replace Temp with Query

---
+++ ReplaceTempWithQuery/order.js
@@ -0,0 +1,21 @@
+class Order {
+    constructor(quantity, item) {
+        this._quantity = quantity;
+        this._item = item;
+    }
+    get price() {
+        return this.basePrice * this.discountFactor;
+    }
+
+    get basePrice() { return this._quantity * this._item.price }
+
+    get discountFactor() {
+        var result = 0.98;
+        if (this.basePrice > 1000) result -= 0.03;
+        return result
+    }
+}
+
+const order = new Order(5, { price: 60 });
+
+console.log(order.price);
