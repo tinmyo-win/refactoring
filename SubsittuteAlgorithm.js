b9b87561641baab46bdfcbaad72e976a25c8ae40
Tin Myo Win <tinmyowin356@gmail.com>
2023-07-06 20:52:18 +0630

    add substitue algorithm

---
+++ SubsittuteAlgorithm.js
@@ -0,0 +1,21 @@
+function foundPerson(people) {
+    const candidates = ["Don", "John", "Kent"];
+    return people.find(p => candidates.includes(p)) || '';
+}
+
+// function foundPerson(people) {
+//     for (let i = 0; i < people.length; i++) {
+//         if (people[i] === "Don") {
+//             return "Don";
+//         }
+//         if (people[i] === "John") {
+//             return "John";
+//         }
+//         if (people[i] === "Kent") {
+//             return "Kent";
+//         }
+//     }
+//     return "";
+// }
+
+console.log("first candidats ", foundPerson(['Don', 'Mary']));
