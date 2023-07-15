90c4123a284b61667a2e170280554a363eb4167c
Tin Myo Win <tinmyowin356@gmail.com>
2023-07-15 22:08:06 +0630

    refactoring with moving statement to callers example

--- MoveStatementToCallers.js
+++ MoveStatementToCallers.js
@@ -2,6 +2,7 @@ function renderPerson(outStream, person) {
     outStream.write(`<p>${person.name}</p>\n`);
     renderPhoto(outStream, person.photo);
     emitPhotoData(outStream, person.photo);
+    outStream.write(`<p>location: ${person.photo.location}</p>\n`);
 }
 
 function renderPhoto(outStream, photo) {
@@ -13,13 +14,14 @@ function listRecentPhotos(outStream, photos) {
         .forEach(p => {
             outStream.write("<div>\n");
             emitPhotoData(outStream, p);
+            outStream.write(`<p>location: ${p.location}</p>\n`);
             outStream.write("</div>\n");
         });
 }
+
 function emitPhotoData(outStream, photo) {
     outStream.write(`<p>title: ${photo.title}</p>\n`);
     outStream.write(`<p>date: ${photo.date.toDateString()}</p>\n`);
-    outStream.write(`<p>location: ${photo.location}</p>\n`);
 }
 
 const person = {
@@ -38,6 +40,6 @@ person.photo = photo;
 
 let write = (data) => console.log(data)
 
-let outStream = {write}
+let outStream = { write }
 
 renderPerson(outStream, person)
