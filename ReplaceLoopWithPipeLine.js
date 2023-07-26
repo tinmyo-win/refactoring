a0403a0803a03eeb4c5ce95387ebcd62f7136032
Tin Myo Win <tinmyowin356@gmail.com>
2023-07-26 15:35:10 +0630

    refactoring with pipeline

--- ReplaceLoopWithPipeLine.js
+++ ReplaceLoopWithPipeLine.js
@@ -3,26 +3,20 @@ const officeData = `office, country, telephone
         Beijing, China, +86 4008 900 505
         Bangalore, India, +91 80 4064 9570
         Porto Alegre, Brazil, +55 51 3079 3550
+
         Chennai, India, +91 44 660 44766`;
 
 function acquireData(input) {
     const lines = input.split("\n");
-    let firstLine = true;
-    const result = [];
 
-    const loopItems = lines;
-    for (const line of loopItems) {
-        if (firstLine) {
-            firstLine = false;
-            continue;
-        }
-        if (line.trim() === "") continue;
-        const record = line.split(",");
-        if (record[1].trim() === "India") {
-            result.push({ city: record[0].trim(), phone: record[2].trim() });
-        }
-    }
-    return result;
+    return lines
+            .slice  (1)
+            .filter (line   => line.trim() !== "")
+            .map    (line   => line.split(","))
+            .filter (fileds => fileds[1].trim() === "India")
+            .map    (fileds => ({ city: fileds[0].trim(), phone: fileds[2].trim() }));
+            ;
+
 }
 
 console.log(acquireData(officeData));
